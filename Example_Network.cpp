7054156c9774204310b1afbe2d8078c8c12a8711
gimhema <49871739+gimhema@users.noreply.github.com>
2022-06-10 12:35:42 +0900

    Update Example_Network.cpp

--- Example_Network.cpp
+++ Example_Network.cpp
@@ -46,11 +46,49 @@ public:
     }
 }
 
+template <typename T>
 auto create_reference_graph(vector<vector<int>> computers)
 {
-  // computers를 기반으로 그래프를 만들어 리턴합니다.
-  // computers vector는 아래와 같이 구성되어있습니다.
-  // [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+    // computers를 기반으로 그래프를 만들어 리턴합니다.
+    // computers vector는 아래와 같이 구성되어있습니다.
+    // [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+    int num_vertice = computers.size();
+    Graph<T> G(num_vertice);
+    map<unsigned, vector<pair<unsigned, T>>> edge_map;
+    
+    // 가중치가 없는 그래프로 간주
+    // num_vertice만큼 반복문을 돌린다.
+    for(int i = 0; i < num_vertice; i++)
+    {
+        for(int j = 0; j < computers[j].size() < j++)
+        {
+            // computers vector 내부에서
+            if(i != j)
+            {
+                // 자기자신은 항상 연결되어있기때문에 제외
+                if(computers[i][j] == 1)
+                {
+                    // i, j가 연결되어있다면 
+                    edge_map[i].push_back({j, 0})
+                }
+            }
+        }
+    }
+    
+    for(auto& i : edge_map)
+    {
+        for(auto& j : i.second)
+        {
+            G.add_edge(Edge<T>({i.first, j.first, j.second}));
+        }
+    }
+    
+    return G;
+}
+
+auto BFS(const Graph<T>& G, unsigned start)
+{
+    
 }
 
 int solution(int n, vector<vector<int>> computers) {
@@ -63,8 +101,8 @@ int solution(int n, vector<vector<int>> computers) {
     //현재 컴퓨터의 네트워크가 구성되었는지를 판단하는 map입니다.
     unordered_map<int, bool> network_complete_map;
     
-    
     // computers vector를 기반으로 그래프를 구성합니다.
+    auto computer_graph = create_reference_graph(comptuers);
     
     // 컴퓨터 인덱스 배열을 computers의 개수만큼 초기화합니다.
     for(int i = 0; i < computers.size(); i++)
@@ -73,18 +111,13 @@ int solution(int n, vector<vector<int>> computers) {
         network_complete_map[i] = false;
     }
     
-    // 컴퓨터 인덱스 배열에 대하여
-    for(int i = 0; i < computer_idx.size(); i++)
-    {
-        if(network_complete_map[i] == false)
-        {
-            // 인덱스가 아직 네트워크에 등록되지않았다면
-            // i번째 인덱스에 대해서 탐색을 진행하며 네트워크를 구성한다.
-            // BFS vs DFS ????       
-        }
-    }
+    // 탐색을 진행한다.
+    BFS(computer_graph, 0);
         
     // 네트워크의 배열을 리턴한다.    
     answer = network.size();
     return answer;
 }
+
+
+
