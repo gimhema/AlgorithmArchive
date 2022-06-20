e812ba316bacb43af7e2ad09cf6c6db7efe8cbfb
gimhema <49871739+gimhema@users.noreply.github.com>
2022-06-20 12:48:57 +0900

    Create boxer.cpp

---
+++ boxer.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 승자를 기준으로 방향 그래프를 그린다.
+// 거쳐가는 노드 개수의 최소값 = 정확하게 순위를 매길 수 있는 선수의 수
+
+int node_count = 9999;
+
+void create_graph(vector<vector<int>> &results)
+{
+    // results를 바탕으로 그래프를 구성한다.
+}
+
+void rank_search()
+{
+    // 그래프를 탐색한다.
+    // 탐색한 간선의 수를 +1 카운트한다.
+    // 결과값을 최소값과 비교하며 최소값을 갱신한다.
+}
+
+int solution(int n, vector<vector<int>> results) {
+    int answer = 0;
+    return answer;
+}
