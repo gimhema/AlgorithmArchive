acdd9b7631c0de1679319e2f249dede87ab26b18
gimhema <49871739+gimhema@users.noreply.github.com>
2022-06-20 12:01:23 +0900

    Update network.cpp

--- network.cpp
+++ network.cpp
@@ -1,13 +1,44 @@
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int num_networks = 0;
 
-void bfs(vector<vector<int>> &computers_ref, i, vector<int> &networks)
+void bfs(vector<vector<int>> &computers_ref, int current_pos, vector<bool> &networks)
 {
     // 탐색 중 다음 노드로 이동 시 networks[next_idx] = true로 돌림
+    // BFS(G, u)
+    queue<int> q;
+    // u.visited = 1
+    networks[current_pos] = true;
+    // q.push(u);
+    q.push(current_pos);
+    // while(q.size()) 
+    while(q.size())
+    {
+        int v = q.front();
+        q.pop();
+        for(int i = 0; i < computers_ref[v].size(); i++)
+        {
+            if(computers_ref[v][i] == 1)
+            {
+                networks[i] = true;
+                q.push(i);
+            }
+        }
+    }
+    //     u = q.front() 
+    //     q.pop()
+    //     for each v ∈ G.Adj[u]
+    //         if v.visited == false
+    //             v.visited = u.visited + 1
+    //             q.push(v)
+    
+    
+    
+    
     
     // 탐색이 종료된 후에는 네트워크의 개수를 1 올림.
     num_networks = num_networks + 1;
@@ -24,7 +55,6 @@ int solution(int n, vector<vector<int>> computers) {
         if(networks[i] != true)
         {
             // 네트워크가 형성이 되지않았다는 가정하에
-            networks[i] = true;
             bfs(computers_cp, i, networks);
         }
     }
