72c10aa21a6c137ee81b8d9433e3b23b25d1ef0b
gimhema <49871739+gimhema@users.noreply.github.com>
2022-06-22 11:17:09 +0900

    Update boxer.cpp

--- boxer.cpp
+++ boxer.cpp
@@ -28,14 +28,37 @@ void create_graph(unordered_map<int, vector<int>> &graph, vector<vector<int>> &r
 void rank_search(unordered_map<int, vector<int>> &graph, int current_pos)
 {
     // if graph에 current_pos를 key로하는 map이 존재하는지
+    // lang_map.contains(key_to_find)
+    if(graph.contains(current_pos))
+    {
         // 존재한다면 node_count ++하고
+        node_count = node_count + 1;
         // for graph[key].size() 만큼
-            // rank_search(graph, graph[current_pos]) 재귀호출
+        for(int i = 0; i < graph[current_pos].size(); i++)
+        {
+            // rank_search(graph, graph[current_pos][i]) 재귀호출
+            rank_search(graph, graph[current_pos][i]);
+        }
+
+    }
     // else 존재하지않는다면, 즉 끝점이라면
+    else
+    {
         // node_count와 node_count_min을 비교
-        // 만약 node_count가 node_count_min보다 작다면 업데이트
-        // 종료
+        // 만약 node_count가 node_count_min보다 작다면 업데이트        
+        count_update();
+    }
+    // 종료
     return;
+}
+
+void count_update()
+{
+    if(node_count < node_count_min)
+    {
+        node_count = node_count_min;
+        node_count = 0;
+    }
 }
 
 
