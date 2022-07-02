f8f07033f42e3863bd999f3455d4addc056f10b8
gimhema <49871739+gimhema@users.noreply.github.com>
2022-07-02 15:24:44 +0900

    Create go_to_school.cpp

---
+++ go_to_school.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 1000000007
+// dfs + 완전탐색
+
+void dfs()
+{
+    // 기저조건 
+    // 1. 집에 도착한 경우 
+    // 2. 범위 밖인 경우 
+}
+
+vector<int> move_right(int current_x, int current _y)
+{
+    return { current_x + 1, current_y };
+}
+
+vector<int> move_up(int current_x, int current _y)
+{
+    return { current_x , current_y + 1 };
+}
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+
+
+    // answer = min_dist % 1000000007
+    return answer;
+}
