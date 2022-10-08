60c29edfd86a9b7a3d84fa4a1f428f54c3e64769
gimhema <49871739+gimhema@users.noreply.github.com>
2022-10-08 23:18:56 +0900

    Create PermutationExample.cpp

---
+++ PermutationExample.cpp
@@ -0,0 +1,23 @@
+// 오름차순
+for(int i = 0; i < n; i++)
+{
+  v.push_back(a[i]);
+}
+
+do
+{
+  // Print or . . . . Logic . . . 
+}while(next_permutation(v.begin(), v.end());
+
+
+// 내림차순
+for(int i = 0; i < n; i++)
+{
+  v.push_back(a[i]);
+}
+
+do
+{
+  // Print or . . . . Logic . . . 
+}while(prev_permutation(v.begin(), v.end());
+
