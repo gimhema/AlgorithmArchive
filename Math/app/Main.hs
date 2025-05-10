bd12d1a6a712eacd22a3f802271a8fb5e6332ec5
kimmo <hemakim0215@naver.com>
2025-05-11 00:36:11 +0900

    .

--- Math/app/Main.hs
+++ Math/app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+-- Functions
+add :: Int -> Int -> Int
+add x y = x + y
+
+mul :: Int -> Int -> Int
+mul x y = x * y
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let add_result = add 1 2
+  let mul_result = mul 3 5
+  putStrLn ("Add Result : " ++ show add_result)
+  putStrLn ("Mul Result : " ++ show mul_result)
