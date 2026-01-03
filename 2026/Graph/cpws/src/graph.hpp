c35c57ed4347ddc4c445ca4bf7c78033620a5b37
kimmo <hemakim0215@naver.com>
2026-01-03 10:40:43 +0900

    .

--- 2026/Graph/cpws/src/graph.hpp
+++ 2026/Graph/cpws/src/graph.hpp
@@ -1,15 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bfs(const vector<vector<int>>& graph, int start) {
+
+    const int MAX = 100000;
+
+    queue<int> result;
+    std::array<bool, MAX> visited = {false};
+
+    visited[start] = true;
+    result.push(start);
+
+    while(!result.empty()) {
+
+        int current = result.front();
+        result.pop();
+
+        for(int child : graph[current]) {
+            if(!visited[child]) {
+                visited[child] = true;
+                result.push(child);
+            }
+        }
+
+    }
 
 
-
-class BFS {
-
-    public:
-
-
-};
-
-class DFS {
-
-    public:
-
-};
+}
