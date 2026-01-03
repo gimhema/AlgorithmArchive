48baa94af14eba9a3b7cefe0c00e70f79035fa51
kimmo <hemakim0215@naver.com>
2026-01-03 10:04:16 +0900

    .

---
+++ 2026/Graph/cpws/src/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+cmake -S . -B build
+cmake --build build -j
+./build/main
+*/
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // solve here
+    return 0;
+}
