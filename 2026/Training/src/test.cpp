8e95bb5cbe6fd49460ea67072a00178f2914f84a
kimmo <hemakim0215@naver.com>
2026-08-29 13:05:04 +0900

    .

--- 2026/Training/src/test.cpp
+++ 2026/Training/src/test.cpp
@@ -1,2 +1,17 @@
+#include "test.h"
 #include "my_memo.h"
 
+void MY_LIB_TEST_PTR::TEST_MY_UNIQUE_PTR()
+{
+
+}
+
+void MY_LIB_TEST_PTR::TEST_MY_SHARED_PTR()
+{
+    
+}
+
+void MY_LIB_TEST_PTR::TEST_MY_WEAK_PTR()
+{
+    
+}
--- 2026/Training/src/test.h
+++ 2026/Training/src/test.h
@@ -1,1 +1,13 @@
 #pragma once
+
+namespace MY_LIB_TEST_PTR
+{
+    void TEST_MY_UNIQUE_PTR();
+    void TEST_MY_SHARED_PTR();
+    void TEST_MY_WEAK_PTR();
+}
+
+namespace MY_LIB_TEST_ASYNC
+{
+    
+}
